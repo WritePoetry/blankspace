3e01c41797137df211ca102126fedf101ae0001f
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-06-10 20:37:05 +0200

    Add classes tht need to be cleaned

---
+++ inc/class-blank-theme.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Blank Theme Class
+ *
+ * @since    0.0.1
+ * @package  Blank_Theme
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Blank_Theme' ) ) :
+
+	/**
+	 * The main Twenties class
+	 */
+	class Blank_Theme {
+
+		/**
+		 * Setup class.
+		 *
+		 * @since 1.0
+		 */
+		public function __construct() {
+			add_action( 'after_setup_theme', array( $this, 'setup' ) );
+			add_action( 'init', array( $this, 'register_block_bindings' ) );
+		}
+
+		/**
+		 * Sets up theme defaults and registers support for various WordPress features.
+		 *
+		 * Note that this function is hooked into the after_setup_theme hook, which
+		 * runs before the init hook. The init hook is too late for some features, such
+		 * as indicating support for post thumbnails.
+		 */
+		public function setup() {
+			/*
+			 * Load Localisation files.
+			 *
+			 * Note: the first-loaded translation file overrides any following ones if the same translation is present.
+			 */
+
+			// Loads wp-content/languages/themes/storefront-it_IT.mo.
+			load_theme_textdomain( 'twenties', trailingslashit( WP_LANG_DIR ) . 'themes' );
+
+			// Loads wp-content/themes/child-theme-name/languages/it_IT.mo.
+			load_theme_textdomain( 'twenties', get_stylesheet_directory() . '/languages' );
+
+			// Loads wp-content/themes/storefront/languages/it_IT.mo.
+			load_theme_textdomain( 'twenties', get_template_directory() . '/languages' );
+
+
+
+
+			/**
+			 * Add support for Block Styles.
+			 */
+			add_theme_support( 'wp-block-styles' );
+
+
+
+			/**
+			 * Add support for editor styles.
+			 */
+			add_theme_support( 'editor-styles' );
+
+
+			/**
+			 * Enqueue editor styles.
+			 */
+			// add_editor_style( 'style.css' );
+
+
+			/**
+			 * Add support for responsive embedded content.
+			 */
+			add_theme_support( 'responsive-embeds' );
+ 		}
+
+
+
+		public function register_block_bindings() {
+			register_block_bindings_source( 'twenties/copyright', array(
+				'label'              => __( 'Copyright', 'twenties' ),
+				'get_value_callback' => array( $this, 'copyright_binding' )
+			) );
+		}
+
+		public function copyright_binding() {
+			return '&copy; ' . date( 'Y' );
+		}
+	}
+endif;
+
+return new Blank_Theme();
+
+
+
+
+/**
+ * Copy the following lines in your functions.php theme file
+ * if you want to remove some particular mime type
+ * added by this plugin
+ */
+
+add_filter(
+	'upload_mimes',
+	function ( $mimes ) {
+		// Optional. Remove a mime type.
+		unset( $mimes['exe'] );
+		unset( $mimes['svg'] );
+		unset( $mimes['svgz'] );
+
+		return $mimes;
+	}
+);
+
+add_filter(
+	'mime_types',
+	function ( $wp_get_mime_types ) {
+		// Optional. Remove a mime type.
+		unset( $wp_get_mime_types['exe'] );
+		unset( $wp_get_mime_types['svg'] );
+		unset( $wp_get_mime_types['svgz'] );
+
+		return $wp_get_mime_types;
+	}
+);
---
+++ inc/class-twenties-ajax.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Storefront NUX Admin Class
+ *
+ * @package  storefront
+ * @since    2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Twenties_Ajax' ) ) :
+
+	/**
+	 * The Twenties Ajax class
+	 */
+	class Twenties_Ajax {
+		/**
+		 * Setup class.
+		 *
+		 * @since 2.2.0
+		 */
+		public function __construct() {
+			add_action( 'wp_ajax_nopriv_pagination', array( $this, 'my_ajax_pagination' ) );
+			add_action( 'wp_ajax_pagination', array( $this, 'my_ajax_pagination' ) );
+			add_action( 'wp_enqueue_scripts', array( $this, 'localize_scripts' ) );
+		}
+
+
+		/**
+		 * Set WooCommerce pages to use the full width template.
+		 *
+		 * @since 2.2.0
+		 */
+		public function localize_scripts() {
+			$params = array(
+				'ajaxurl' => admin_url( 'admin-ajax.php' ),
+				'data_var_1' => 'value 1',
+				'data_var_2' => 'value 2',
+			);
+
+			wp_localize_script( 'twenties-frontend-ajax', 'twenties_frontend_ajax_object', $params );
+		}
+
+		public function my_ajax_pagination() {
+
+			echo get_bloginfo( 'title' );
+			die();
+
+		}
+
+
+	}
+
+endif;
+
+return new Twenties_Ajax();
