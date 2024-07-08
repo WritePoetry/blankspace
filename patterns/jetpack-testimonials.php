a2cfb0f3e34f2c8818de6fa547c356ca28237848
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-07-08 18:07:07 +0200

    Update testimonials layout and content display

--- patterns/jetpack-testimonials.php
+++ patterns/jetpack-testimonials.php
@@ -9,10 +9,18 @@
 <!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background has-link-color">
 	
+	<!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"small"} -->
+	<p class="has-text-align-center has-primary-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase">Testimonials</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:heading {"textAlign":"center"} -->
+	<h2 class="wp-block-heading has-text-align-center">What our client think about us</h2>
+	<!-- /wp:heading -->
+		
 	<!-- wp:html -->
 	<?php
 		// print the newsletter shortcode this way to prevent the <p> tags from being added
-	 	echo do_shortcode( '[testimonials]' );
+	 	echo do_shortcode( '[testimonials columns=2 display_content=full]' );
 	?>
 	<!-- /wp:html -->
 </div>
