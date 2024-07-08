4b1a56a8ecfe6a430628898f3ae3018655586d64
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-07-08 18:08:38 +0200

    refactor: Update screen.js to use forEach instead of querySelectorAll

--- resources/js/screen.js
+++ resources/js/screen.js
@@ -1,59 +1,46 @@
 /**
  * WordPress dependencies
  */
-// import { store, getContext } from '@wordpress/interactivity';
 import domReady from '@wordpress/dom-ready';
 
 const initialize = () => {
 	'use strict';
 
-	const root = document.querySelector( '.api-fetch' );
+	const apiFetch = document.getElementsByClassName( 'api-fetch' );
 
-	if ( ! root ) {
+	if ( ! apiFetch.length ) {
 		console.error( 'Repo element not found' );
 		return;
 	}
+	
+	Array.from( apiFetch ).forEach(( root ) => {
+		
+		// Get repo from data attribute
+		const url = root.getAttribute( 'data-api-url' );
+		const link = root.getAttribute( 'data-api-link' );
+		const text = root.getAttribute( 'data-api-text' );
 
-	// Get repo from data attribute
-	const url = root.getAttribute( 'data-api-url' );
-	const link = root.getAttribute( 'data-api-link' );
-	const text = root.getAttribute( 'data-api-text' );
+		// Get the github repo latest release version from GitHub
+		fetch( url )
+			.then( ( response ) => response.json() )
+			.then( ( data ) => {
+				const element = document.createElement( 'a' );
 
-	// Get the github repo latest release version from GitHub
-	fetch( url )
-		.then( ( response ) => response.json() )
-		.then( ( data ) => {
-			const element = document.createElement( 'a' );
+				if ( ! element ) {
+					console.error( 'Version element not found' );
+					return;
+				}
 
-			if ( ! element ) {
-				console.error( 'Version element not found' );
-				return;
-			}
+				element.textContent = data[ text ];
+				element.href = data[ link ];
 
-			element.textContent = data[ text ];
-			element.href = data[ link ];
-
-			root.appendChild( element );
-		} )
-		.catch( ( error ) => console.error( error ) );
+				root.appendChild( element );
+			} )
+			.catch( ( error ) => console.error( error ) );
+	});
 };
 
 if ( typeof window !== 'undefined' ) {
 	domReady( initialize );
 }
 
-// store( 'create-block', {
-// 	actions: {
-// 		toggle: () => {
-// 			const context = getContext();
-// 			context.isOpen = ! context.isOpen;
-// 		},
-// 	},
-// 	callbacks: {
-// 		logIsOpen: () => {
-// 			const { isOpen } = getContext();
-// 			// Log the value of `isOpen` each time it changes.
-// 			console.log( `Is open: ${ isOpen }` );
-// 		},
-// 	},
-// } );
