2bfae8eebad537141c862dff2d12c09d10795d30
Giacomo Secchi <giacomo.secchi@gmail.com>
2024-08-31 18:21:11 +0200

    Update blog-home.php layout and styles
    The blog-home.php file has been updated to improve the layout and styles of the blog section. This includes removing unnecessary code and adjusting the alignment of elements. These changes enhance the visual presentation and user experience of the blog page.

--- patterns/blog-home.php
+++ patterns/blog-home.php
@@ -7,66 +7,44 @@
  * Block Types: core/buttons
  */
 ?>
-
-
-	 
 	<?php 
 		// Get the title of the posts page
 		$page_ID = get_option( 'page_for_posts' );
 	?>
-	
-	
-
 		
  	<!-- wp:spacer {"height":"var(--wp--preset--spacing--60)"} -->
 	 <div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
 	 
-	        <!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","fontSize":"small"} -->
-			<p class="has-text-align-center has-secondary-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase"><?php echo esc_html_x( 'Blog & news', 'writewhite' ); ?></p>
-        <!-- /wp:paragraph -->
-
-		
+	<!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"textColor":"secondary","fontSize":"small"} -->
+		<p class="has-text-align-center has-secondary-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase"><?php echo esc_html_x( 'Blog & news', 'writewhite' ); ?></p>
+	<!-- /wp:paragraph -->
 		 
-		<!-- wp:heading {"textAlign":"center","align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"primary"} -->
-        <h2 class="wp-block-heading alignwide has-text-align-center has-primary-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0"><?php get_template_part( 'template-parts/content/page-title' , null , $page_ID ); ?></h2>
-        <!-- /wp:heading -->
-		 
-		 
-		
-
-		
+	<!-- wp:heading {"textAlign":"center","align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"primary"} -->
+		<h2 class="wp-block-heading alignwide has-text-align-center has-primary-color has-text-color has-link-color" style="margin-top:0;margin-bottom:0"><?php get_template_part( 'template-parts/content/page-title' , null , $page_ID ); ?></h2>
+	<!-- /wp:heading -->
 
 	
-
-		
  	<!-- wp:spacer {"height":"var(--wp--preset--spacing--60)"} -->
 	 <div style="height:var(--wp--preset--spacing--60)" aria-hidden="true" class="wp-block-spacer"></div>
 	<!-- /wp:spacer -->
 
-	
+	<!-- wp:query {"query":{"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"displayLayout":{"type":"flex","columns":3},"align":"wide","layout":{"type":"constrained"}} -->
+	<div class="wp-block-query alignwide">
 		 
 		 <!-- wp:post-template {"align":"wide","layout":{"type":"grid","columnCount":3}} -->
  
-		 <!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
-		 <div class="wp-block-group has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+			<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-white-background-color has-background" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
 
-
-			 <!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"clamp(15vw, 30vh, 400px)","align":"wide","style":{"border":{"radius":"15px"}}} /-->
-			 <!-- wp:post-title {"isLink":true} /-->
-			 <!-- wp:post-excerpt /-->
-			 <!-- wp:post-date {"isLink":true} /-->
-			 <!-- wp:read-more /-->
-		 
-
-		</div>
-		<!-- /wp:group -->
-		
-
-			  
-
- 
-		 <!-- /wp:post-template -->
+				<!-- wp:post-featured-image {"isLink":true,"width":"100%","height":"clamp(15vw, 30vh, 400px)","align":"wide","style":{"border":{"radius":"15px"}}} /-->
+				<!-- wp:post-title {"isLink":true} /-->
+				<!-- wp:post-excerpt /-->
+				<!-- wp:post-date {"isLink":true} /-->
+				<!-- wp:read-more /-->
+			</div>
+			<!-- /wp:group --> 
+		<!-- /wp:post-template -->
 	
 		
  
