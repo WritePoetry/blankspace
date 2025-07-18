0422564c1cd7634d05fd66763f92903fe8eee30d
Alberto-Giusti <spaghetti_boys@hotmail.it>
2025-07-18 18:04:29 +0200

    Add className to main navigation block for improved styling

--- patterns/nav-main.php
+++ patterns/nav-main.php
@@ -4,4 +4,4 @@
  * Slug: blankspace/nav-main
  */
 ?>
-<!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontWeight":"400","textTransform":"uppercase"},"spacing":{"blockGap":"var:preset|spacing|60"}},"fontSize":"small","layout":{"type":"flex","justifyContent":"center"},"setCascadingProperties":true,"ariaLabel":"<?php esc_attr_e( 'Main Menu', 'blankspace' ); ?>"} /-->
+<!-- wp:navigation {"overlayMenu":"never","className":"main-navigation","style":{"typography":{"fontWeight":"400","textTransform":"uppercase"},"spacing":{"blockGap":"var:preset|spacing|60"}},"fontSize":"small","layout":{"type":"flex","justifyContent":"center"},"setCascadingProperties":true,"ariaLabel":"<?php esc_attr_e( 'Main Menu', 'blankspace' ); ?>"} /-->
