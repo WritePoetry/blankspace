cdae70d952db4de37319dbaab56dc864b7e9bfcb
Alberto-Giusti <spaghetti_boys@hotmail.it>
2025-07-21 10:58:57 +0200

    Fix indentation and formatting in media-rounded-image-right pattern

--- patterns/media-rounded-image-right.php
+++ patterns/media-rounded-image-right.php
@@ -7,7 +7,9 @@
  */
 ?>
 <!-- wp:media-text {"align":"wide","mediaPosition":"right","mediaLink":"<?php echo esc_url( get_template_directory_uri() ); ?>/photos/whatsapp-image-2021-03-07-at-15-43-35-e1615129102707/","mediaType":"image","imageFill":false,"className":"is-style-rounded-image","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}}} -->
-<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-style-rounded-image" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><div class="wp-block-media-text__content"><!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"small"} -->
+<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile is-style-rounded-image" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)">
+<div class="wp-block-media-text__content">
+<!-- wp:paragraph {"align":"left","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"small"} -->
 <p class="has-text-align-left has-primary-color has-text-color has-link-color has-small-font-size" style="text-transform:uppercase">Lorem Ipsum</p>
 <!-- /wp:paragraph -->
 
@@ -17,6 +19,8 @@
 
 <!-- wp:paragraph {"placeholder":"Content…"} -->
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus commodo nunc sodales tincidunt pellentesque. Sed arcu sem, vestibulum id sagits vel, rutrum vel ligula. In ornare lectus vel consequat pulvinar. Vestibulum porttitor eleifend interdum. Nam ac orci et libero mollis iaculis vitae in sapien. Pellentesque lobortis ultrices est, a molestie massa convallis pretium.</p>
-<!-- /wp:paragraph --></div><figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/pexels-andrew-3178818.jpg" alt="" class="size-full"/></figure></div>
-<!-- /wp:media-text -->
+<!-- /wp:paragraph -->
 
+</div>
+<figure class="wp-block-media-text__media"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/pexels-andrew-3178818.jpg" alt=""/></figure></div>
+<!-- /wp:media-text -->
