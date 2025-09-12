398c968634a86fcc68c0a57e71af42be1d933f1b
Giacomo Secchi <giacomosecchi@gmail.com>
2025-09-12 14:30:08 +0200

    Refactor Brevo newsletter subscribe pattern and update button border radius in theme.json

--- patterns/newsletter-subscribe-brevo.php
+++ patterns/newsletter-subscribe-brevo.php
@@ -6,13 +6,12 @@
  * Keywords: newsletter, subscribe, button
  */
 ?>
-<!-- wp:group {"backgroundColor":"contrast","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-contrast-background-color has-background">
 
-	<!-- wp:html -->
-	<?php
-		// print the newsletter shortcode this way to prevent the <p> tags from being added
-	 	echo do_shortcode( '[sibwp_form id=1]' );
-	?>
-	<!-- /wp:html --></div>
-	<!-- /wp:group -->
+<!-- wp:group {"className":"subscribe-newsletter-brevo"} -->
+<div class="wp-block-group subscribe-newsletter-brevo">
+	<!-- wp:shortcode -->
+[sibwp_form id=<?php echo esc_attr( defined('BREVO_FORM_ID') ? BREVO_FORM_ID : 1 ); ?>]
+<!-- /wp:shortcode -->
+</div>
+<!-- /wp:group -->
+
